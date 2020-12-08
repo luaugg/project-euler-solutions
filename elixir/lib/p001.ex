c49499432fea527c26cf8f1ed7be53cec1a005d7
Sam Pritchard <sampritchard0801@gmail.com>
2020-12-08 03:05:17 +0000

    Small refactor

--- elixir/lib/p001.ex
+++ elixir/lib/p001.ex
@@ -1,10 +1,21 @@
 # Find the sum of all multiples of 3 or 5 below 1000
 
 defmodule P001 do
-  def sum(1000, state), do: state
-  def sum(value, state) when rem(value, 3) == 0, do: sum(value + 1, state + value)
-  def sum(value, state) when rem(value, 5) == 0, do: sum(value + 1, state + value)
-  def sum(value, state), do: sum(value + 1, state)
+  @moduledoc false
+
+  defp sum(1000, state), do: state
+
+  defp sum(value, state) when rem(value, 3) == 0 do
+    sum(value + 1, state + value)
+  end
+
+  defp sum(value, state) when rem(value, 5) == 0 do
+    sum(value + 1, state + value)
+  end
+
+  defp sum(value, state), do: sum(value + 1, state)
+
+  def sum, do: sum(3, 0)
 end
 
-IO.puts P001.sum(3, 0)
+IO.puts P001.sum
--- elixir/lib/p002.ex
+++ elixir/lib/p002.ex
@@ -1,13 +1,16 @@
 # Sum of even-valued Fibonacci numbers less than four million.
 
 defmodule P002 do
-  def sum(_, value, state) when value >= 4000000, do: state
+  @moduledoc false
+  defp sum(_, value, state) when value >= 4000000, do: state
 
-  def sum(previous, value, state) when rem(value, 2) == 0 do
+  defp sum(previous, value, state) when rem(value, 2) == 0 do
     sum(value, previous + value, state + value)
   end
 
-  def sum(previous, value, state), do: sum(value, previous + value, state)
+  defp sum(previous, value, state), do: sum(value, previous + value, state)
+
+  def sum, do: sum(1, 1, 0)
 end
 
-IO.puts P002.sum(1, 1, 0)
+IO.puts P002.sum
