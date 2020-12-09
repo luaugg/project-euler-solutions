f775d9d02ab1fc55b8dffdf4554d8bba46ab1d97
Sam Pritchard <sampritchard0801@gmail.com>
2020-12-09 03:02:37 +0000

    Problem #007 solution

---
+++ elixir/lib/p007.ex
@@ -0,0 +1,43 @@
+# Find 10,001st prime number
+# We will just use trial division for this. Sieves would also work but they're more
+# complicated to implement.
+
+# We only need to check prime factors up to the square root of the number.
+
+defmodule P007 do
+  @moduledoc false
+
+  defp divide_inner(value, divisor) do
+    if rem(value, divisor) == 0 do
+      {:halt, divisor}
+    else
+      {:cont, divisor}
+    end
+  end
+
+  def divide(10001, value, _),
+    do: value - 2
+
+  def divide(index, value, divisors) do
+    # Only checking primes up to the square root.
+    sqrt = :math.sqrt(value)
+    reversed = divisors
+      |> Enum.reverse
+      |> Enum.filter(&(&1 <= sqrt))
+
+    reduced = Enum.reduce_while(reversed, 0, fn divisor, _ -> divide_inner(value, divisor) end)
+
+    if rem(value, reduced) != 0 do
+        # prime branch
+        divide(index + 1, value + 2, [value | divisors])
+    else
+        # composite branch
+        divide(index, value + 2, divisors)
+    end
+  end
+
+  def start,
+    do: divide(2, 5, [3, 2])
+end
+
+# IO.inspect P007.start, label: "10,001st prime number"
