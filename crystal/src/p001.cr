ad378d0abc6750ea588050295547698a844f6e2c
Sam Pritchard <sampritchard0801@gmail.com>
2020-12-10 22:14:28 +0000

    Use if-as-an-expression for p001.cr

--- crystal/src/p001.cr
+++ crystal/src/p001.cr
@@ -4,12 +4,10 @@ def sum
   sum = 0
 
   (1..1000).each do |i|
-    if i % 3 == 0 || i % 5 == 0
-      sum += i    
-    end
+    sum += i if i % 3 == 0 || i % 5 == 0
   end
 
   sum
 end
 
-# puts sum
+puts sum
