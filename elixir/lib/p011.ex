6ffb85ded37a6606a7212834eae4714048b5b751
Sam Pritchard <sampritchard0801@gmail.com>
2020-12-13 20:01:36 +0000

    Problem #11 Elixir solution

---
+++ elixir/lib/p011.ex
@@ -0,0 +1,43 @@
+# Find greatest product of four adjacent numbers in same direction in <grid>.
+# There are probably more efficient ways to do this but /shrug.
+# The matrix ETF data is in the resources folder. It is just a map of index to grid value.
+
+defmodule P011 do
+  @moduledoc false
+
+  defp product(map, index, largest, fun) do
+    1 .. 3
+      |> Enum.map(&Map.get(map, fun.(&1)))
+      |> Enum.reject(&is_nil/1)
+      |> Enum.reduce(Map.get(map, index), &(&1 * &2))
+      |> max(largest)
+  end
+
+  def largest_product(_map, 400, largest),
+    do: largest
+  def largest_product(map, index, largest) do
+    # UP/DOWN/LEFT/RIGHT
+    largest = product(map, index, largest, &(index - &1 * 20))
+    largest = product(map, index, largest, &(index + &1 * 20))
+    largest = product(map, index, largest, &(index - &1))
+    largest = product(map, index, largest, &(index + &1))
+
+    # DIAGONALS
+    largest = product(map, index, largest, &(index - &1 * 19))
+    largest = product(map, index, largest, &(index + &1 * 19))
+    largest = product(map, index, largest, &(index - &1 * 21))
+    largest = product(map, index, largest, &(index + &1 * 21))
+
+    largest_product(map, index + 1, largest)
+  end
+
+  def start do
+    {:ok, etf} = File.read("resources/p011_map.data")
+
+    etf
+      |> :erlang.binary_to_term
+      |> largest_product(0, 0)
+  end
+end
+
+# IO.inspect P011.start, label: "Product"
